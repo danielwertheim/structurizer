8689c295f5161d6a486396611a53526a54cf6e53
Daniel Wertheim <daniel@wertheim.se>
2016-11-09 21:38:11 +0100

    Sample

---
+++ src/Sample/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using Structurizer;
+using Structurizer.Configuration;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order
+            {
+                Id = 1,
+                OrderSimpleInts = new[] { 1, 2, 3 },
+                OrderNo = "2016-1234",
+                PlacedAt = DateTime.Now,
+                Lines = new List<OrderLine>
+                {
+                    new OrderLine
+                    {
+                        OrderLineSimpleInts = new [] {11,12,13},
+                        ArticleNo = "Article-Line0",
+                        Qty = 42,
+                        Items =  new List<Item>
+                        {
+                            new Item
+                            {
+                                Name = "Key-Line0-Item0",
+                                Value = "Value-Line0-Item0",
+                                ItemSimpleInts = new [] {101, 102, 103}
+                            },
+                            new Item
+                            {
+                                Name = "Key-Line0-Item1",
+                                Value = "Value-Line0-Item1"
+                            }
+                        }
+                    },
+                    new OrderLine
+                    {
+                        ArticleNo = "Article-Line1",
+                        Qty = 3,
+                        Items =  new List<Item>
+                        {
+                            new Item
+                            {
+                                Name = "Key-Line1-Item0",
+                                Value = "Value-Line1-Item0"
+                            },
+                            new Item
+                            {
+                                Name = "Key-Line1-Item1",
+                                Value = "Value-Line1-Item1"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var typeConfigs = new StructureTypeConfigurations();
+            typeConfigs.Register<Order>();
+            typeConfigs.Register<Foo>();
+
+            //typeConfigs.Register<Order>(cfg => cfg
+            //    .UsingIndexMode(IndexMode.Inclusive)
+            //    .Members(i => i.OrderNo));
+            //.Members(i => i.Lines[0].Items[0].Name));
+
+            //inclusive:Lines.*
+            //inclusive:Lines.ArticleNo
+            //exclusive:Lines.*
+            //exclusive.Lines.ArticleNo
+
+            var structureBuilder = StructureBuilder.Create(typeConfigs);
+            var orderStructure = structureBuilder.CreateStructure(order);
+            var fooStructure = structureBuilder.CreateStructure(new Foo
+            {
+                FooScore = 1,
+                Bar = new Bar
+                {
+                    BarScore = new[] { 2, 3 }
+                }
+            });
+
+            Console.WriteLine($"===== {orderStructure.Name} =====");
+            foreach (var index in orderStructure.Indexes)
+                Console.WriteLine(DefaultIndexValueFormatter.Format(index));
+
+            Console.WriteLine($"===== {fooStructure.Name} =====");
+            foreach (var index in fooStructure.Indexes)
+                Console.WriteLine(DefaultIndexValueFormatter.Format(index));
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class DefaultIndexValueFormatter
+    {
+        public static string Format(IStructureIndex index)
+        {
+            var path = index.Path;
+
+            switch (index.DataTypeCode)
+            {
+                case DataTypeCode.String:
+                case DataTypeCode.Guid:
+                case DataTypeCode.Enum:
+                    return $"{path}=\"{index.Value}\"";
+                case DataTypeCode.DateTime:
+                    return $"{path}=\"{((DateTime)index.Value):O}\"";
+                default:
+                    return $"{path}={index.Value}";
+            }
+        }
+    }
+
+    public interface IOrder
+    {
+        int Id { get; set; }
+        string OrderNo { get; set; }
+        DateTime PlacedAt { get; set; }
+        List<OrderLine> Lines { get; set; }
+        int[] OrderSimpleInts { get; set; }
+    }
+
+    public class Order : IOrder
+    {
+        public int Id { get; set; }
+        public string OrderNo { get; set; }
+        public DateTime PlacedAt { get; set; }
+        public List<OrderLine> Lines { get; set; }
+        public int[] OrderSimpleInts { get; set; }
+    }
+
+    public class OrderLine
+    {
+        public string ArticleNo { get; set; }
+        public int Qty { get; set; }
+        public List<Item> Items { get; set; }
+        public int[] OrderLineSimpleInts { get; set; }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public int[] ItemSimpleInts { get; set; }
+    }
+
+    public class Foo
+    {
+        public int FooScore { get; set; }
+        public Bar Bar { get; set; }
+    }
+
+    public class Bar
+    {
+        public int[] BarScore { get; set; }
+    }
+}
---
+++ src/Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApplication1")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e921c1c6-8dc3-4ace-9c0e-480ef1bdfe4f")]

4be84538f0cdffb59973c4ea5688f1b19ef7f0c2
Daniel Wertheim <daniel@wertheim.se>
2016-11-27 13:17:00 +0100

    Delegate

--- src/projects/Structurizer/Schemas/DynamicGetter.cs
+++ src/projects/Structurizer/Schemas/DynamicGetter.cs
@@ -1,16 +1,4 @@
-using System;
-
 namespace Structurizer.Schemas
 {
-    public class DynamicGetter
-    {
-        private readonly Func<object, object> _accessor;
-
-        public DynamicGetter(Func<object, object> accessor)
-        {
-            _accessor = accessor;
-        }
-
-        public object GetValue<T>(T item) => _accessor(item);
-    }
+    public delegate object DynamicGetter(object obj);
 }
